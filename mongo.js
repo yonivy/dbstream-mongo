a74313c31203047a681919601cf01cef5100683c
Yoni Levy <yoni@panoply.io>
2023-12-17 17:59:36 +0200

    Removing excessive whitespace and minor linting

--- mongo.js
+++ mongo.js
@@ -1,50 +1,50 @@
 const debug = require('debug')('dbstream-mongo');
-const mongodb = require( 'mongodb' );
-const events = require( 'events' );
-const extend = require( 'extend' );
-const db = require( 'dbstream' );
-const util = require( 'util' );
+const mongodb = require('mongodb');
+const events = require('events');
+const extend = require('extend');
+const db = require('dbstream');
+const util = require('util');
 
-module.exports.mongodb = mongodb
-module.exports.ObjectID = mongodb.ObjectId
+module.exports.mongodb = mongodb;
+module.exports.ObjectID = mongodb.ObjectId;
 
-const CLOSE_TIME_OUT = 10 * 60 * 1000 // 10 minutes
+const CLOSE_TIME_OUT = 10 * 60 * 1000; // 10 minutes
 
-util.inherits( Cursor, db.Cursor );
-function Cursor( conn ) {
-    Cursor.super_.call( this );
+util.inherits(Cursor, db.Cursor);
+function Cursor(conn) {
+    Cursor.super_.call(this);
     this._conn = conn;
 }
 
-Cursor.prototype._save = function ( object, callback ) {
-    this._conn.open( function ( err, collection ) {
-        if ( err ) return callback( toError( err ) );
+Cursor.prototype._save = function (object, callback) {
+    this._conn.open(function (err, collection) {
+        if (err) return callback(toError(err));
 
         var conn = this;
-        var serialized = replace( {}, object );
-        if ( typeof serialized.id != 'undefined' ) {
-            serialized._id = toObjectID( serialized.id );
+        var serialized = replace({}, object);
+        if (typeof serialized.id != 'undefined') {
+            serialized._id = toObjectID(serialized.id);
             delete serialized.id;
         }
 
-        if ( !serialized._id ) {
-            collection.insertOne( serialized, ondone )
+        if (!serialized._id) {
+            collection.insertOne(serialized, ondone);
         } else {
             const filter = { '_id': serialized._id }
             const options = { upsert: true }
-            collection.replaceOne( filter,  serialized, options, ondone );
+            collection.replaceOne(filter, serialized, options, ondone);
         }
 
-        function ondone ( err, result ) {
+        function ondone(err, result) {
             conn.done();
-            if ( err ) return callback( toError( err ) );
-            if ( typeof result == 'object' ) {
-                if ( result.ops && result.ops.length > 0 ) {
-                    result = result.ops[0]
+            if (err) return callback(toError(err));
+            if (typeof result == 'object') {
+                if (result.ops && result.ops.length > 0) {
+                    result = result.ops[0];
                 }
-                result.id = fromObjectID( result._id );
+                result.id = fromObjectID(result._id);
                 delete result._id;
-                replace( object, result );
+                replace(object, result);
             }
             callback();
         }
@@ -52,60 +52,60 @@ Cursor.prototype._save = function ( object, callback ) {
     });
 };
 
-Cursor.prototype._remove = function ( object, callback ) {
-    if ( !object.id ) {
+Cursor.prototype._remove = function (object, callback) {
+    if (!object.id) {
         var msg = 'Unable to remove object without an ID';
-        return callback( new Error( msg ) );
+        return callback(new Error(msg));
     }
 
-    var id = toObjectID( object.id );
-    this._conn.open( function ( err, collection ) {
-        if ( err ) return callback( toError( err ) );
+    var id = toObjectID(object.id);
+    this._conn.open(function (err, collection) {
+        if (err) return callback(toError(err));
         var conn = this;
-        collection.remove({ _id: id }, function ( err ) {
+        collection.remove({ _id: id }, function (err) {
             conn.done();
-            callback( toError( err ) );
+            callback(toError(err));
         })
     });
 };
 
 Cursor.prototype._load = function () {
-    if ( this._reading ) return;
+    if (this._reading) return;
     this._reading = true;
 
     var options = {};
-    if ( this._limit ) options.limit = this._limit;
-    if ( this._skip )  options.skip = this._skip;
-    if ( this._sort )  options.sort = ( this._sort || [] ).map( function ( s ) {
-        return [ s.key, s.direction ];
+    if (this._limit) options.limit = this._limit;
+    if (this._skip) options.skip = this._skip;
+    if (this._sort) options.sort = (this._sort || []).map(function (s) {
+        return [s.key, s.direction];
     });
 
-    var query = replace( {}, this._query );
-    if ( query.id ) {
-        query._id = toObjectID( query.id );
+    var query = replace({}, this._query);
+    if (query.id) {
+        query._id = toObjectID(query.id);
         delete query.id;
     }
 
     var that = this;
-    this._conn.open( function ( err, collection ) {
-        if ( err ) return this.emit( 'error', toError( err ) );
+    this._conn.open(function (err, collection) {
+        if (err) return this.emit('error', toError(err));
         var conn = this;
         collection
-            .find( query, options )
+            .find(query, options)
             .stream()
-            .on( 'error', function ( err ) {
+            .on('error', function (err) {
                 conn.done();
-                that.emit( 'error', toError( err ) );
+                that.emit('error', toError(err));
             })
-            .on( 'end', function () {
+            .on('end', function () {
                 conn.done();
-                that.push( null );
+                that.push(null);
                 that._reading = false;
             })
-            .on( 'data', function ( obj ) {
-                obj.id = fromObjectID( obj._id );
+            .on('data', function (obj) {
+                obj.id = fromObjectID(obj._id);
                 delete obj._id;
-                that.push( obj );
+                that.push(obj);
             });
     });
 }
@@ -113,19 +113,19 @@ Cursor.prototype._load = function () {
 // dbstream API requires that objects be modified in-place, and not
 // replaced completely with a new reference in order to allow users to access
 // the modified object
-function replace ( obj, other ) {
+function replace(obj, other) {
     var name;
 
     // remove non-existing keys
-    for ( name in obj ) {
-        if ( typeof other[ name ] == 'undefined' ) {
-            delete obj[ name ];
+    for (name in obj) {
+        if (typeof other[name] == 'undefined') {
+            delete obj[name];
         }
     }
 
     // set new keys
-    for ( name in other ) {
-        obj[ name ] = other[ name ];
+    for (name in other) {
+        obj[name] = other[name];
     }
 
     return obj;
@@ -134,20 +134,20 @@ function replace ( obj, other ) {
 
 var connections = {};
 
-function closeClient( url, options ) {
-    if ( !connections[ url ] || !connections[ url ].client ) {
+function closeClient(url, options) {
+    if (!connections[url] || !connections[url].client) {
         debug('connection does not exist');
         return;
     }
 
-    connections[ url ].clients -= 1;
-    if ( connections[ url ].clients > 0 ) {
+    connections[url].clients -= 1;
+    if (connections[url].clients > 0) {
         debug('cannot close connection due to active clients for', url);
         return; // still has other open clients
     }
 
     // already scheduled to be closed
-    if ( connections[ url ].closeTimeout ) {
+    if (connections[url].closeTimeout) {
         debug('closing connection later, timeout already exists for', url);
         return;
     }
@@ -158,26 +158,26 @@ function closeClient( url, options ) {
         closeTimeOut = timeOut
     }
 
-    connections[ url ].closeTimeout = setTimeout( function () {
+    connections[url].closeTimeout = setTimeout(function () {
         debug('timeout reached, closing', url);
-        connections[ url ].client.close()
-        delete connections[ url ];
-    }, closeTimeOut );
+        connections[url].client.close()
+        delete connections[url];
+    }, closeTimeOut);
 }
 
-function getClient ( url, options, callback ) {
+function getClient(url, options, callback) {
     let retry = options.retry
     let maxRetries = options.maxRetries
     delete options.retry
     delete options.maxRetries
 
     // not connected at all
-    if ( !connections[ url ] ) {
+    if (!connections[url]) {
         debug('connecting to', url);
-        connections[ url ] = { callbacks: [ callback ], clients: 1 }
-        mongodb.MongoClient.connect( url, options, function ready( err, client ) {
-            if ( !err && client ) {
-                connections[ url ].client = client;
+        connections[url] = { callbacks: [callback], clients: 1 }
+        mongodb.MongoClient.connect(url, options, function ready(err, client) {
+            if (!err && client) {
+                connections[url].client = client;
             }
 
             var shouldRetry = retry < maxRetries
@@ -187,35 +187,35 @@ function getClient ( url, options, callback ) {
                 return mongodb.MongoClient.connect(url, options, ready)
             }
 
-            connections[ url ].callbacks.forEach( function ( callback ) {
-                callback.call( null, toError( err ), client );
+            connections[url].callbacks.forEach(function (callback) {
+                callback.call(null, toError(err), client);
             })
         });
         return;
     }
 
-    clearTimeout( connections[ url ].closeTimeout );
-    delete connections[ url ].closeTimeout;
+    clearTimeout(connections[url].closeTimeout);
+    delete connections[url].closeTimeout;
 
-    debug('adding clients to', url, ' - clients count:', connections[ url ].clients + 1);
-    connections[ url ].clients += 1;
+    debug('adding clients to', url, ' - clients count:', connections[url].clients + 1);
+    connections[url].clients += 1;
 
     // already running, subscribe to get the connection callback
-    if ( !connections[ url ].client ) {
-        connections[ url ].callbacks.push( callback );
+    if (!connections[url].client) {
+        connections[url].callbacks.push(callback);
         return
     }
 
     // already connected
-    callback( null, connections[ url ].client );
+    callback(null, connections[url].client);
 }
 
-module.exports.connect = function( url, options ) {
+module.exports.connect = function (url, options) {
     const collection = options.collection;
     delete options.collection;
 
-    if ( !collection ) {
-        throw new Error( 'options.collection is required' );
+    if (!collection) {
+        throw new Error('options.collection is required');
     }
 
     // Default maxRetries value is 1 for backwards compatibility
@@ -224,71 +224,71 @@ module.exports.connect = function( url, options ) {
     options.retry = 1;
 
     var conn = new events.EventEmitter();
-    conn.open = function ( callback ) {
-        getClient( url, options, function ( err, client ) {
-            if ( err ) {
-                callback.call( conn, err, null )
+    conn.open = function (callback) {
+        getClient(url, options, function (err, client) {
+            if (err) {
+                callback.call(conn, err, null)
                 return
             }
             const db = client.db()
-            callback.call( conn, null, db.collection( collection ) );
+            callback.call(conn, null, db.collection(collection));
         })
     }
     conn.done = function () {
-        closeClient( url, options );
+        closeClient(url, options);
     }
 
-    util.inherits( _Cursor, Cursor );
+    util.inherits(_Cursor, Cursor);
     function _Cursor() {
-        _Cursor.super_.call( this, conn );
+        _Cursor.super_.call(this, conn);
     }
 
     conn.Cursor = _Cursor;
     return conn;
 }
 
-module.exports.destroy = function() {
-    for ( const url in connections ) {
+module.exports.destroy = function () {
+    for (const url in connections) {
         debug('closing', url);
 
-        clearTimeout( connections[ url ].closeTimeout )
-        delete connections[ url ].closeTimeout;
+        clearTimeout(connections[url].closeTimeout)
+        delete connections[url].closeTimeout;
 
-        if( connections[ url ].client ) {
-            connections[ url ].client.close();
+        if (connections[url].client) {
+            connections[url].client.close();
         }
-        delete connections[ url ];
+        delete connections[url];
 
         debug('deleted', url);
     }
 }
 
-function toObjectID( id ) {
+function toObjectID(id) {
     if (id.$in) {
         id.$in = id.$in.map(toObjectID);
         return id;
     }
 
-    if ( typeof id == 'string' && id.length == 24 && mongodb.ObjectId.isValid( id ) ) {
-        return new mongodb.ObjectId( id );
+    if (typeof id == 'string' && id.length == 24 && mongodb.ObjectId.isValid(id)) {
+        return new mongodb.ObjectId(id);
     } else {
         return id;
     }
 }
 
-function fromObjectID( id ) {
-    if ( mongodb.ObjectId.isValid(id) ) {
+function fromObjectID(id) {
+    if (mongodb.ObjectId.isValid(id)) {
         return id.toString();
     } else {
         return id;
     }
 }
 
-function toError( err ) {
-    if ( !err ) return;
-    if ( err instanceof Error ) return err;
+function toError(err) {
+    if (!err) return;
+    if (err instanceof Error) return err;
 
     // mongo sdk might return an object with the format { err: message }
     // instead of a proper Javascript Error instance
-    return extend( new Error( err.message || err.err ), err );
+    return extend(new Error(err.message || err.err), err);
 }
